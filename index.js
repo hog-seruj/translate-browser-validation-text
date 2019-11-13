2590c5b2000c001f85d555bf0977c3f942a8ade6
HOG <hogseruj1@gmail.com>
2019-11-13 14:41:58 +0200

    Added format text validation.

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ Drupal.behaviors.formValidationTranslate = {
         languages: { // Language key must be the same as in Drupal.
           fi: 'Ole hyvä ja täydennä kenttä',
           et: 'Palun täida väli',
+          lt: 'Prašome užpildyti šį laukelį',
+          lv: 'Lūdzu, aizpildiet',
           en: 'Please fill in this field'
         },
         text: Drupal.t('Please fill in this field'),
+        textFormat: Drupal.t('The format is not correct'),
         type: 'text'
       },
       {
@@ -16,6 +19,8 @@ Drupal.behaviors.formValidationTranslate = {
         languages: { // Language key must be the same as in Drupal.
           fi: 'Ole hyvä ja täydennä kenttä',
           et: 'Palun täida väli',
+          lt: 'Prašome užpildyti šį laukelį',
+          lv: 'Lūdzu, aizpildiet.',
           en: 'Please fill in this field'
         },
         text: Drupal.t('Please fill in this field'),
@@ -23,11 +28,11 @@ Drupal.behaviors.formValidationTranslate = {
       },
     ];
     for (var i = 0; i < elements.length; i++) {
-      this.init(elements[i].el, elements[i].text, elements[i].type);
+      this.init(elements[i].el, elements[i].text, elements[i].textFormat, elements[i].type);
     }
   },
-  init: function (el, text, type) {
-    this.customValidation(el, text, type);
+  init: function (el, text, textFormat, type) {
+    this.customValidation(el, text, textFormat, type);
   },
   translate: function (el, languages) {
     for (var thisLanguage in languages) {
@@ -36,17 +41,22 @@ Drupal.behaviors.formValidationTranslate = {
       }
     }
   },
-  customValidation: function (elements, text, type) {
+  customValidation: function (elements, text, textFormat, type) {
     [].forEach.call(elements, function (el) {
       el.oninvalid = function (e) {
         e.target.setCustomValidity("");
-        if (e.target.validity.valueMissing) {
-          e.target.setCustomValidity(text);
+        if (type === 'text') {
+          if (e.target.validity.valueMissing) {
+            e.target.setCustomValidity(text);
+          }
+          if (e.target.validity.patternMismatch) {
+            e.target.setCustomValidity(textFormat);
+          }
         }
         else if (type === 'email') {
           let reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           if (!reg.test(String(el.value).toLowerCase())) {
-            let patternText = Drupal.t('Please type correctly the email address, @value not equal email format "name@domain.com".', {'@value': el.value});
+            let patternText = Drupal.t('Please correct the email address field, since @value does not have the right format "name@domain.com".', {'@value': el.value});
             e.target.setCustomValidity(patternText);
             inputValue = el.value;
           }
